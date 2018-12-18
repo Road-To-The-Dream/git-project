c237738cdeb8cec795b0da5ee8495ef02530a4fd
Sergey <rpz14.srgey@gmail.com>
2018-12-18 20:54:23 +0200

    Create Main View

--- practice/Router/index.php
+++ practice/Router/index.php
@@ -1,8 +1,24 @@
 <?php
     class Route
     {
+        public function myAutoload($className)
+        {
+            $class_pieces = explode('/', $className);
+            switch ($className[0])
+            {
+                case 'Model':
+                    require_once __DIR__.'/Model'.implode(DIRECTORY_SEPARATOR, $class_pieces).'.php';
+                    break;
+                case 'Controller':
+                    require_once __DIR__.'/Controller'.implode(DIRECTORY_SEPARATOR, $class_pieces).'.php';
+                    break;
+            }
+        }
+
         public function Routing($url)
         {
+            spl_autoload_register($this->myAutoload(), true);
+
             $action_name = '';
             $view_name= '';
             $id = 1;
@@ -16,7 +32,7 @@
             }
 
             // get name action
-            if ( !empty($routes[2]) )                        //show_all
+            if ( !empty($routes[2]) )                       //show_all
             {
                 $action_name = strtolower($routes[2]);
             }
