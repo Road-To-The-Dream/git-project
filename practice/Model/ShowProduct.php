6783192f23579adcf8b73706a2611d8420d0077a
Sergey <rpz14.srgey@gmail.com>
2019-01-06 16:55:32 +0200

    Refactor connection DB

--- practice/Model/ShowProduct.php
+++ practice/Model/ShowProduct.php
@@ -7,7 +7,7 @@
  */
 
 namespace practice\Model;
-
+use PDO;
 
 class ShowProduct
 {
@@ -26,12 +26,48 @@ class ShowProduct
     private function connect()
     {
         $dsn = 'mysql:dbname=' . $this->settings['dbname'] . ';host=' . $this->settings['host'];
+        try {
+            $this->pdo = new \PDO($dsn, $this->settings['user'], $this->settings['password'], [\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES ' .$this->settings['charset']]);
+
+            # Disable emulations and we can now log
+            $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $this->pdo->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+            $this->isConnected = true;
+            $this->init('Select name From client');
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    public function closeConnection()
+    {
+        $this->pdo = null;
+    }
+
+    private function init($query, array $parameters = [])
+    {
+        if(!$this->isConnected) {
+            $this->connect();
+        }
 
         try {
-            $this->pdo = new \PDO($dsn, $this->settings['user'], $this->settings['password']);
+            $this->statement = $this->pdo->prepare($query);
+
+            //$this->bind($parameters);
         } catch (\PDOException $ex) {
             exit($ex->getMessage());
         }
     }
 
+//    private function bind($parameters)
+//    {
+//        if(!empty($parameters) and is_array($parameters)) {
+//            $columns = array_keys($parameters);
+//
+//            foreach ($columns as $i)
+//        }
+//    }
 }
+$config = require_once 'config.php';
+ $obj = new ShowProduct($config['db']);
+
---
+++ practice/Model/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sergey
+ * Date: 06.01.2019
+ * Time: 16:11
+ */
+    return [
+        'db' => [
+            'host' => 'localhost',
+            'dbname' => 'Sample',
+            'user' => 'root',
+            'password' => '',
+            'charset' => 'utf8'
+        ]
+    ];
+
