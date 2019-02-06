176a3f4b6df96b5178fead1830bc39118a0605d2
Sergey <rpz14.srgey@gmail.com>
2019-02-06 20:57:47 +0200

    Finished work on the file

--- practice/tests/Model/ConnectionManagerTest.php
+++ practice/tests/Model/ConnectionManagerTest.php
@@ -17,18 +17,27 @@ class ConnectionManagerTest extends TestCase
         $this->assertIsObject(ConnectionManager::getInstance());
     }
 
-    public function testExecutionQuery()
+    public function testSelect()
     {
         $query_select = "SELECT id, last_name, first_name, patronymic, email, phone, password, role FROM client";
         $this->assertIsArray(ConnectionManager::executionQuery($query_select));
+    }
 
+    public function testInsert()
+    {
         $query_insert = "INSERT INTO comments (content, date_added, create_at, client_id, product_id)
                   VALUES ('Second Comment','2019-02-04 22:58:33','2019-02-04 22:58:33', 2, 1)";
         $this->assertTrue(ConnectionManager::executionQuery($query_insert));
+    }
 
+    public function testDelete()
+    {
         $query_delete = "DELETE FROM comments WHERE id = 24";
         $this->assertTrue(ConnectionManager::executionQuery($query_delete));
+    }
 
+    public function testUpdate()
+    {
         $query_update = "UPDATE client SET
                                     last_name = 'Богданов',
                                     first_name = 'Дмитрий',
