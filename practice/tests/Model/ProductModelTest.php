e2b431da59ace1366a2bd6a06a02613b13fff658
Sergey <rpz14.srgey@gmail.com>
2019-02-08 22:18:44 +0200

    Finished work on the file

--- practice/tests/Model/ProductModelTest.php
+++ practice/tests/Model/ProductModelTest.php
@@ -9,11 +9,73 @@
 namespace practice\Controller;
 
 use PHPUnit\Framework\TestCase;
+use practice\Model\ActiveRecord\Product;
 
 class ProductModelTest extends TestCase
 {
+    private $obj;
+
+    protected function setUp()
+    {
+        $this->obj = new Product();
+        $this->obj->setId(1);
+    }
+
+    public function testGetAmountProducts()
+    {
+        $result = $this->obj->getAmountProducts("", 1);
+
+        $this->assertEquals(8, $result[0]['amt']);
+    }
+
     public function testSelectProduct()
     {
+        $result = $this->obj->selectProduct();
 
+        $this->assertEquals(1, $result[0]->getId());
+        $this->assertEquals('Lenovo ThinkPad Edge E470 (20H1006YRT)', $result[0]->getName());
+        $this->assertEquals('20 499', $result[0]->getPrice());
+        $this->assertEquals('грн', $result[0]->getUnit());
+        $this->assertEquals(1, $result[0]->getAmount());
+    }
+
+    public function testSelectName()
+    {
+        $result = $this->obj->selectName(array(8));
+
+        $this->assertEquals('MSI GT75-8RF Titan (GT758RF-239UA)', $result[0]->getName());
+    }
+
+    public function testSelectPrice()
+    {
+        $result = $this->obj->selectPriceProduct();
+
+        $this->assertEquals('20499', $result[0]['price']);
+    }
+
+    public function testSelectSearch()
+    {
+        $this->obj->setName('Lenovo ThinkPad Edge');
+        $result = $this->obj->selectProductSearch();
+
+        $this->assertEquals(1, $result[0]['id']);
+        $this->assertEquals('Lenovo ThinkPad Edge E470 (20H1006YRT)', $result[0]['name']);
+    }
+
+    public function testupdateDecreaseAmount()
+    {
+        $this->assertEquals(true, $this->obj->updateDecreaseAmount());
+    }
+
+    public function testupdateIncreaseAmount()
+    {
+        $this->obj->setAmount(1);
+        $this->obj->setUpdateAt('\'' . date("Y-m-d H:i:s") . '\'');
+        $this->assertEquals(true, $this->obj->updateIncreaseAmount());
+    }
+
+    public function testDelete()
+    {
+        $this->assertEquals(true, $this->obj->delete());
     }
 }
