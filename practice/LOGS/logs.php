1b073491b19c578fc854876bf4ac518110dca435
Sergey <rpz14.srgey@gmail.com>
2019-02-09 08:03:04 +0200

    Refactor file

--- practice/LOGS/logs.php
+++ practice/LOGS/logs.php
@@ -5,3 +5,8 @@
  * Date: 08.02.2019
  * Time: 23:07
  */
+
+ini_set('display_errors', false);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/logs.txt');
+error_reporting(E_ALL & ~E_NOTICE);
